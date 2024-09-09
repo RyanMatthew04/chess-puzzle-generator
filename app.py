1390f66d832fed45065e2c331404b761ee82bae2
RyanMatthew04 <ryananthonymatthew@gmail.com>
2024-09-09 20:20:56 +0530

    first commit

--- app.py
+++ app.py
@@ -10,10 +10,23 @@ def index():
 @app.route('/generate_fen', methods=['GET'])
 def generate_fen():
     # Run your Python script and get the FEN string
-    result = subprocess.run(['python', 'generate.py'], capture_output=True, text=True)
-    fen_string = result.stdout.strip()
+    try:
+        result = subprocess.run(['python', 'generate.py'], capture_output=True, text=True)
+        fen_string = result.stdout.strip()
+
+        if not fen_string:
+                raise ValueError("Received empty FEN from generate.py")
+            
+        return jsonify({'fen': fen_string})
     
-    return jsonify({'fen': fen_string})
+    except subprocess.CalledProcessError as e:
+        print(f"Subprocess error: {e}")
+        return jsonify({'fen': e}), 500
+    except ValueError as e:
+        print(f"Value error: {e}")
+        return jsonify({'fen': e}), 500
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
